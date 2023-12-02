a4b23da9655515e472202fb5ff998ce2d430d1c9
mathilde-01 <mathilde.7892@gmail.com>
2023-12-02 14:16:26 -0500

    added shapes and unit testing

--- index.js
+++ index.js
@@ -4,50 +4,110 @@ const inquirer = require("inquirer");
 // File system module (node package manager) import
 const fs = require("fs");
 
-
 // Questions prompt using inquirer
 function promptQuestions() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message:
-            "Enter your logo text (Up to three characters)",
-          name: "text",
-        },
-        {
-          type: "input",
-          message:
-            "Choose your text color",
-          name: "textColor",
-        },
-        {
-          type: "list",
-          message: "Choose your shape",
-          choices: ["Circle", "Triangle", "Square"],
-          name: "shape",
-        },
-        {
-          type: "input",
-          message:
-            "Choose your shape background color",
-          name: "shapeBackgroundColor",
-        },
-      ])
-      .then((answers) => {
-        // Error for text prompt length
-        if (answers.text.length > 3) {
-          console.log("Must enter a value of no more than 3 characters");
-          promptQuestions();
-        } else {
-          // Calling write file function to generate SVG file
-          writeToFile("logo.svg", answers);
-        }
-      });
-  }
-  
-  // Calling promptUser function so inquirer prompts fire off when application is ran
-  promptQuestions();
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter text for your logo (Up to three characters)",
+        name: "text",
+      },
+      {
+        type: "input",
+        message: "Choose the text color (OR a hexadecimal number)",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "Choose the shape",
+        choices: ["Circle", "Triangle", "Square"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message: "Choose the shape background color",
+        name: "shapeBackgroundColor",
+      },
+    ])
+    .then((answers) => {
+      // Error for text prompt length
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptQuestions();
+      }
+      if (answers.shape == "Circle") {
+        // Calling write file function to generate SVG file
+        fs.writeFileSync(
+          "./dist/logo.svg",
+          `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
+  <circle cx="150" cy="100" r="80" fill="${answers.shapeBackgroundColor}" />
 
-  // Function writeToFile
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+
+</svg>
+          `
+        );
+      }
+      if (answers.shape == "Triangle") {
+        // Calling write file function to generate SVG file
+        fs.writeFileSync(
+          "./dist/logo.svg",
+          `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}" />
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+
+</svg>
+          `
+        );
+      }
+      if (answers.shape == "Square") {
+        // Calling write file function to generate SVG file
+        fs.writeFileSync(
+          "./dist/logo.svg",
+          `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+  <rect width="200" height="200" fill="${answers.shapeBackgroundColor}" />
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+
+</svg>
+          `
+        );
+      }
+      
+    });
+}
+
+// Calling promptUser function so inquirer prompts fire off when application is ran
+promptQuestions();
+
+//   // Function writeToFile
+//   function writeToFile(fileName, answers) {
+//     fs.writeFile(fileName, data, (err) => {
+//       err ? console.error(err) : console.log("Your SVG file was created");
+//     });
+//   }
+
+//   // Function to initialize app
+// function init() {
+//     inquirer
+//       .prompt(questions)
+//       .then((answers) => {
+//       //   console.log(answers);
+//         const readmeContent = generateMarkdown(answers);
+//         writeToFile("README.md", readmeContent);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   }
+
+// Function call to initialize app
+// init();
